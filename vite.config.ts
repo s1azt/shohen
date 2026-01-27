3e3ad7acc17e37e1787cbeb89f9dd2911119a08b
mhara1-ctrl <mhara1@nekonet.co.jp>
2026-01-27 18:07:21 +0900

    feat:20260127

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,20 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  // サーバー上の公開パスに合わせる
+    plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  base: '/gs-intra-new/docs/', 
+  build: {
+    outDir: 'docs', // ビルド先をdocsフォルダに指定
+  },
+  // ...
+})
+
+/*
+export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
@@ -13,4 +27,4 @@ export default defineConfig({
     emptyOutDir: true, // ビルドのたびに docs フォルダ内をきれいに掃除する
   },
   base: './', // GitHub Pages などで画像やCSSのパスがずれないようにする設定
-})
+}) */
