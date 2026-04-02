02debb0e03f48aca08fcb6e80599683bb78d3e68
mhara1-ctrl <mhara1@nekonet.co.jp>
2026-04-02 13:32:39 +0900

    20260402午後本番環境反

--- src/data/news-auto.ts
+++ src/data/news-auto.ts
@@ -1,8 +1,32 @@
 // このファイルは scripts/fetch-news.mjs によって自動生成されます
 // 手動で編集しないでください（上書きされます）
-// 最終取得: 2026/4/2 9:00:01
+// 最終取得: 2026/4/2 13:30:01
 
 export const autoNews = [
+  {
+    "id": 648140128,
+    "date": "2026.04.02",
+    "category": "教育/研修関連",
+    "title": "【Gemini通信】Gemini × 営業：明日から使える、成果が上がる Gemini 営業術",
+    "color": "text-orange-500",
+    "url": "http://tokyo.nekonet.co.jp/menu/pageout.aspx?key=Whats&url=http://dominoap.nekonet.co.jp/all/all0088.nsf/($All)/2B06E08B7CA2422E49258DCD000A3A24"
+  },
+  {
+    "id": 682932445,
+    "date": "2026.04.02",
+    "category": "人事関連",
+    "title": "55期有休取得80％促進の取組について",
+    "color": "text-pink-600",
+    "url": "http://tokyo.nekonet.co.jp/menu/pageout.aspx?key=Whats&url=http://dominoap.nekonet.co.jp/all/all0088.nsf/($All)/DC0FA0D91F37676A49258DCD0005674D"
+  },
+  {
+    "id": 1927377077,
+    "date": "2026.04.02",
+    "category": "セキュリティ/危機管理",
+    "title": "Symantecの定義ファイルを更新してください",
+    "color": "text-red-500",
+    "url": "http://tokyo.nekonet.co.jp/menu/pageout.aspx?key=Whats&url=http://ungi.nekonet.co.jp/security/virus/download.html"
+  },
   {
     "id": 1878464532,
     "date": "2026.04.01",
@@ -58,29 +82,5 @@ export const autoNews = [
     "title": "【ご案内】SMART SECURE HABITS 4月号～情報セキュリティの新習慣～",
     "color": "text-red-500",
     "url": "http://tokyo.nekonet.co.jp/menu/pageout.aspx?key=Whats&url=http://ungi.nekonet.co.jp/security/committee/SmartSecureHabits/202604.pdf"
-  },
-  {
-    "id": 1632885613,
-    "date": "2026.04.01",
-    "category": "通達",
-    "title": "経営戦略部(CSR)通達5501　「業界団体に関する各手続について」",
-    "color": "text-red-500",
-    "url": "http://tokyo.nekonet.co.jp/menu/pageout.aspx?key=Whats&url=http://dominoap.nekonet.co.jp/all/all0088.nsf/($All)/65AE1A8156492CFF49258DCC000C3479"
-  },
-  {
-    "id": 73840207,
-    "date": "2026.04.01",
-    "category": "教育/研修関連",
-    "title": "［情報共有］2027年度　IPA情報処理技術者試験　変更予定について",
-    "color": "text-orange-500",
-    "url": "http://tokyo.nekonet.co.jp/menu/pageout.aspx?key=Whats&url=http://dominoap.nekonet.co.jp/all/all0088.nsf/($All)/F4693DE00BCD903B49258DCC000A12DA"
-  },
-  {
-    "id": 764093164,
-    "date": "2026.04.01",
-    "category": "社内業務",
-    "title": "【第55期】組織変更に伴う各マスタ登録内容の変更処理について",
-    "color": "text-slate-600",
-    "url": "http://tokyo.nekonet.co.jp/menu/pageout.aspx?key=Whats&url=http://dominoap.nekonet.co.jp/all/all0088.nsf/($All)/C0C906A27955DAD249258DCC000795E7"
   }
 ];
