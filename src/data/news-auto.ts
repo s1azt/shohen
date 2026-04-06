89cf6c48179493ecf213d946d9ab6d67505a9dc7
mhara1-ctrl <mhara1@nekonet.co.jp>
2026-04-06 09:00:09 +0900

    auto: news update 2026/04/06  9:00:09.03

--- src/data/news-auto.ts
+++ src/data/news-auto.ts
@@ -1,8 +1,40 @@
 // このファイルは scripts/fetch-news.mjs によって自動生成されます
 // 手動で編集しないでください（上書きされます）
-// 最終取得: 2026/4/3 13:30:04
+// 最終取得: 2026/4/6 9:00:03
 
 export const autoNews = [
+  {
+    "id": 693687882,
+    "date": "2026.04.06",
+    "category": "セキュリティ/危機管理",
+    "title": "【今週も安全・快適に！】ブラウザを最新にしてリフレッシュ★",
+    "color": "text-red-500",
+    "url": "http://tokyo.nekonet.co.jp/menu/pageout.aspx?key=Whats&url=http://dominoap.nekonet.co.jp/all/all0088.nsf/($All)/DBF0B548474A858949258DA70083B71D"
+  },
+  {
+    "id": 449850968,
+    "date": "2026.04.03",
+    "category": "社内業務",
+    "title": "【ソフトウェアライセンス管理】組織改正対応のお知らせ_4/13(月)9:00-11:00",
+    "color": "text-slate-600",
+    "url": "http://tokyo.nekonet.co.jp/menu/pageout.aspx?key=Whats&url=http://dominoap.nekonet.co.jp/all/all0088.nsf/($All)/EEF4B230AFE98C5949258DCE0024172E"
+  },
+  {
+    "id": 1156439376,
+    "date": "2026.04.03",
+    "category": "社内業務",
+    "title": "【収入印紙】4月10日（金）の出庫停止について",
+    "color": "text-slate-600",
+    "url": "http://tokyo.nekonet.co.jp/menu/pageout.aspx?key=Whats&url=http://dominoap.nekonet.co.jp/all/all0088.nsf/($All)/06C5E9F309D4297549258DCE0020B915"
+  },
+  {
+    "id": 567865927,
+    "date": "2026.04.03",
+    "category": "人事関連",
+    "title": "【2026年度上期資格奨励金】 対象資格が決定しました",
+    "color": "text-pink-600",
+    "url": "http://tokyo.nekonet.co.jp/menu/pageout.aspx?key=Whats&url=http://dominoap.nekonet.co.jp/all/all0088.nsf/($All)/28EC910FDB8B977749258DCD001707AD"
+  },
   {
     "id": 34375555,
     "date": "2026.04.02",
@@ -50,37 +82,5 @@ export const autoNews = [
     "title": "Symantecの定義ファイルを更新してください",
     "color": "text-red-500",
     "url": "http://tokyo.nekonet.co.jp/menu/pageout.aspx?key=Whats&url=http://ungi.nekonet.co.jp/security/virus/download.html"
-  },
-  {
-    "id": 1878464532,
-    "date": "2026.04.01",
-    "category": "セキュリティ/危機管理",
-    "title": "フィッシングメール情報を更新しました ※要Chromeアクセス",
-    "color": "text-red-500",
-    "url": "http://tokyo.nekonet.co.jp/menu/pageout.aspx?key=Whats&url=https://sites.google.com/nekonet.co.jp/phising-info?usp=sharing"
-  },
-  {
-    "id": 1702828597,
-    "date": "2026.04.01",
-    "category": "社内業務",
-    "title": "新販売管理システムの稼働スケジュールおよび運用開始について",
-    "color": "text-slate-600",
-    "url": "http://tokyo.nekonet.co.jp/menu/pageout.aspx?key=Whats&url=http://dominoap.nekonet.co.jp/all/all0088.nsf/($All)/8A276FA2516917C449258DCC000E5252"
-  },
-  {
-    "id": 969674840,
-    "date": "2026.04.01",
-    "category": "人事関連",
-    "title": "課別勤怠一覧・勤怠情報公開の更新について",
-    "color": "text-pink-600",
-    "url": "http://tokyo.nekonet.co.jp/menu/pageout.aspx?key=Whats&url=http://dominoap.nekonet.co.jp/all/all0088.nsf/($All)/FD3807618474885749258DCC0024A12B"
-  },
-  {
-    "id": 831560171,
-    "date": "2026.04.01",
-    "category": "人事関連",
-    "title": " （4/9(木)17:00まで）【契約社員システム】2026年4月度給与反映分のマスタ更新について",
-    "color": "text-red-500",
-    "url": "http://tokyo.nekonet.co.jp/menu/pageout.aspx?key=Whats&url=http://dominoap.nekonet.co.jp/all/all0088.nsf/($All)/461B35888042922949258DCA00322DBD/"
   }
 ];
