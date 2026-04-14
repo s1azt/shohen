f43b316b058498e3c01444a9eabe685151799b4b
mhara1-ctrl <mhara1@nekonet.co.jp>
2026-04-14 09:59:47 +0900

    20260414締め切り

--- src/data/deadlines-auto.ts
+++ src/data/deadlines-auto.ts
@@ -1,49 +1,21 @@
 // このファイルは scripts/fetch-deadlines.mjs によって自動生成されます
 // 手動で編集しないでください（上書きされます）
-// 最終取得: 2026/4/9 14:35:48
+// 最終取得: 2026/4/14 9:59:15
 
 import type { DeadlineItem } from "./deadlines";
 
 export const autoDeadlines: DeadlineItem[] = [
-  {
-    "id": 750420880,
-    "title": "（本日12:00まで）【正社員】4月度住所・通勤費申請の申請期日について",
-    "date": "2026.04.09",
-    "updateDate": "2026.04.09",
-    "url": "http://tokyo.nekonet.co.jp/menu/pageout.aspx?key=Whats&url=http://dominoap.nekonet.co.jp/all/all0088.nsf/($All)/CDEBA375A99010B049258DCA0031D583/",
-    "bg": "bg-[#fff5f5]",
-    "border": "border-[#feb2b2]",
-    "text": "text-[#742a2a]",
-    "btn": "bg-[#e53e3e]",
-    "accent": "border-l-[#e53e3e]",
-    "iconName": "Clipboard",
-    "dept": "人事関連"
-  },
-  {
-    "id": 1314274756,
-    "title": "【4/9(木)15時まで】期越え前の承認期限について_保守・資産システム（OBIC7）",
-    "date": "2026.04.09",
-    "updateDate": "2026.04.08",
-    "url": "http://tokyo.nekonet.co.jp/menu/pageout.aspx?key=Whats&url=http://dominoap.nekonet.co.jp/all/all0088.nsf/($All)/21C1A5BC3E2B3AB549258DD3002C79FF",
-    "bg": "bg-[#f0fdf4]",
-    "border": "border-[#bbf7d0]",
-    "text": "text-[#166534]",
-    "btn": "bg-[#059669]",
-    "accent": "border-l-[#059669]",
-    "iconName": "Clipboard",
-    "dept": "調達関連"
-  },
   {
     "id": 1378474706,
     "title": "4月23日 経営戦略会議 議案募集の件　[締切：4月14日]",
     "date": "2026.04.23",
     "updateDate": "2026.04.09",
     "url": "http://tokyo.nekonet.co.jp/menu/pageout.aspx?key=Whats&url=http://dominoap.nekonet.co.jp/all/all0088.nsf/($All)/1616CDDF00BE64A249258DD3000DB026",
-    "bg": "bg-[#f0f9ff]",
-    "border": "border-[#bae6fd]",
-    "text": "text-[#075985]",
-    "btn": "bg-[#0284c7]",
-    "accent": "border-l-[#0284c7]",
+    "bg": "bg-[#fff5f5]",
+    "border": "border-[#feb2b2]",
+    "text": "text-[#742a2a]",
+    "btn": "bg-[#e53e3e]",
+    "accent": "border-l-[#e53e3e]",
     "iconName": "Clipboard",
     "dept": "経営方針/制度"
   }
