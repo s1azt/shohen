efb263bbdf26eb23f742f094f5e094df6793ba96
git-s-tada <s_tada@nekonet.co.jp>
2026-04-21 15:16:21 +0900

    更新

--- src/data/deadlines-auto.ts
+++ src/data/deadlines-auto.ts
@@ -1,22 +1,36 @@
 // このファイルは scripts/fetch-deadlines.mjs によって自動生成されます
 // 手動で編集しないでください（上書きされます）
-// 最終取得: 2026/4/15 13:50:20
+// 最終取得: 2026/4/21 15:15:59
 
 import type { DeadlineItem } from "./deadlines";
 
 export const autoDeadlines: DeadlineItem[] = [
   {
-    "id": 59247167,
-    "title": "【保守】4月の工番/経費負担部署/設置場所変更の期限について【4/17申請〆・4/20AM承認〆】",
-    "date": "2026.04.17",
-    "updateDate": "2026.04.14",
-    "url": "http://tokyo.nekonet.co.jp/menu/pageout.aspx?key=Whats&url=http://dominoap.nekonet.co.jp/all/all0088.nsf/($All)/017AA8F9BA03012149258DD9002F2F7D",
+    "id": 1655688525,
+    "title": "2026年夏季粗品　ご注文について【〆切：4月28日（火）】",
+    "date": "2026.04.28",
+    "updateDate": "2026.04.20",
+    "url": "http://tokyo.nekonet.co.jp/menu/pageout.aspx?key=Whats&url=http://dominoap.nekonet.co.jp/all/all0088.nsf/($All)/8E3AEBAFCEE70FE049258DDC00206E2B",
     "bg": "bg-[#fff5f5]",
     "border": "border-[#feb2b2]",
     "text": "text-[#742a2a]",
     "btn": "bg-[#e53e3e]",
     "accent": "border-l-[#e53e3e]",
     "iconName": "Clipboard",
-    "dept": "調達関連"
+    "dept": "営業情報"
+  },
+  {
+    "id": 218220136,
+    "title": "5月8日 経営戦略会議 議案募集の件　[締切：4月23日]",
+    "date": "2026.05.08",
+    "updateDate": "2026.04.20",
+    "url": "http://tokyo.nekonet.co.jp/menu/pageout.aspx?key=Whats&url=http://dominoap.nekonet.co.jp/all/all0088.nsf/($All)/9615D316FA85837449258DDC00076B97",
+    "bg": "bg-[#f0fdf4]",
+    "border": "border-[#bbf7d0]",
+    "text": "text-[#166534]",
+    "btn": "bg-[#059669]",
+    "accent": "border-l-[#059669]",
+    "iconName": "Clipboard",
+    "dept": "経営方針/制度"
   }
 ];
