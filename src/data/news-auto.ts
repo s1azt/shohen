6020464005555cd5aff67e1bdb1b07137e12969e
mhara1-ctrl <mhara1@nekonet.co.jp>
2026-04-28 09:00:09 +0900

    auto: news update 2026/04/28  9:00:09.17

--- src/data/news-auto.ts
+++ src/data/news-auto.ts
@@ -1,8 +1,56 @@
 // このファイルは scripts/fetch-news.mjs によって自動生成されます
 // 手動で編集しないでください（上書きされます）
-// 最終取得: 2026/4/24 13:30:04
+// 最終取得: 2026/4/28 9:00:02
 
 export const autoNews = [
+  {
+    "id": 2011862994,
+    "date": "2026.04.27",
+    "category": "社内業務",
+    "title": "完了【ソフトウェアライセンス管理システム】メンテナンスのお知らせ（本日 18:00～）",
+    "color": "text-slate-600",
+    "url": "http://tokyo.nekonet.co.jp/menu/pageout.aspx?key=Whats&url=http://dominoap.nekonet.co.jp/all/all0088.nsf/($All)/AE671DB6926AD10B49258DE5007ED156"
+  },
+  {
+    "id": 1433967341,
+    "date": "2026.04.27",
+    "category": "セキュリティ/危機管理",
+    "title": "★情報セキュリティ便り4月号★(守るべき4つのキホン/ルール改定/GWの注意喚起)　",
+    "color": "text-red-500",
+    "url": "http://tokyo.nekonet.co.jp/menu/pageout.aspx?key=Whats&url=http://ungi.nekonet.co.jp/security/committee/securitynews/202604.pdf"
+  },
+  {
+    "id": 1406603883,
+    "date": "2026.04.27",
+    "category": "セキュリティ/危機管理",
+    "title": "【今週も安全・快適に！】ブラウザを最新にしてリフレッシュ★",
+    "color": "text-red-500",
+    "url": "http://tokyo.nekonet.co.jp/menu/pageout.aspx?key=Whats&url=http://dominoap.nekonet.co.jp/all/all0088.nsf/($All)/DBF0B548474A858949258DA70083B71D"
+  },
+  {
+    "id": 1054443695,
+    "date": "2026.04.24",
+    "category": "社内業務",
+    "title": "【重要】新販売管理システム（OBIC7）移行に伴うパスワードリセットおよびID整理のお知らせ",
+    "color": "text-red-500",
+    "url": "http://tokyo.nekonet.co.jp/menu/pageout.aspx?key=Whats&url=http://dominoap.nekonet.co.jp/all/all0088.nsf/($All)/98DBF6E19D7DE28949258DE3002CC70F"
+  },
+  {
+    "id": 1388585052,
+    "date": "2026.04.24",
+    "category": "セキュリティ/危機管理",
+    "title": "(4/24新規)【即時対応必須】ブラウザのアップデート実施をお願いします。",
+    "color": "text-red-500",
+    "url": "http://tokyo.nekonet.co.jp/menu/pageout.aspx?key=Whats&url=https://drive.google.com/file/d/1v9qiUgahTyuUe6FEMTZ6uz3NNF-w4AXN/view?usp=drive_link"
+  },
+  {
+    "id": 1724154210,
+    "date": "2026.04.24",
+    "category": "人事関連",
+    "title": "【育児支援】「ベビーシッター割引券」・「ベビカム報告」のご案内",
+    "color": "text-pink-600",
+    "url": "http://tokyo.nekonet.co.jp/menu/pageout.aspx?key=Whats&url=http://dominoap.nekonet.co.jp/all/all0088.nsf/($All)/FBFDDE340877985E49258DDC001EADA9"
+  },
   {
     "id": 326410126,
     "date": "2026.04.24",
@@ -34,53 +82,5 @@ export const autoNews = [
     "title": "(4/23新規)【即時対応必須】ブラウザのアップデート実施をお願いします。",
     "color": "text-red-500",
     "url": "http://tokyo.nekonet.co.jp/menu/pageout.aspx?key=Whats&url=https://drive.google.com/file/d/1O8LiyVwT2gFX7QtQwc0mmWcW0Qzz56Tk/view?usp=drive_link"
-  },
-  {
-    "id": 352420490,
-    "date": "2026.04.23",
-    "category": "セキュリティ/危機管理",
-    "title": "Symantecの定義ファイルを更新してください",
-    "color": "text-red-500",
-    "url": "http://tokyo.nekonet.co.jp/menu/pageout.aspx?key=Whats&url=http://ungi.nekonet.co.jp/security/virus/download.html"
-  },
-  {
-    "id": 1891541765,
-    "date": "2026.04.22",
-    "category": "社内業務",
-    "title": "定期点検完了のお知らせ",
-    "color": "text-slate-600",
-    "url": "http://tokyo.nekonet.co.jp/menu/pageout.aspx?key=Whats&url=http://dominoap.nekonet.co.jp/all/all0088.nsf/($All)/16168B2ABADCE5C449258DDB001D1A9B"
-  },
-  {
-    "id": 418905827,
-    "date": "2026.04.22",
-    "category": "人事関連",
-    "title": "通勤のみに自家用車両を利用している場合の指定書類の提出について（申請DB新設のお知らせ）",
-    "color": "text-pink-600",
-    "url": "http://tokyo.nekonet.co.jp/menu/pageout.aspx?key=Whats&url=http://dominoap.nekonet.co.jp/all/all0088.nsf/($All)/542FAAA29FCBB1F449258DDC002802E7/"
-  },
-  {
-    "id": 556004850,
-    "date": "2026.04.22",
-    "category": "セキュリティ/危機管理",
-    "title": "【脆弱性対応】シンクラ通信機器メンテナンスのご案内",
-    "color": "text-red-500",
-    "url": "http://tokyo.nekonet.co.jp/menu/pageout.aspx?key=Whats&url=http://dominoap.nekonet.co.jp/all/all0088.nsf/($All)/4FA02123C8E8F50349258DCA0014F64F"
-  },
-  {
-    "id": 334012365,
-    "date": "2026.04.22",
-    "category": "セキュリティ/危機管理",
-    "title": "フィッシングメール情報を更新しました ※要Chromeアクセス",
-    "color": "text-red-500",
-    "url": "http://tokyo.nekonet.co.jp/menu/pageout.aspx?key=Whats&url=https://sites.google.com/nekonet.co.jp/phising-info?usp=sharing"
-  },
-  {
-    "id": 131301139,
-    "date": "2026.04.21",
-    "category": "セキュリティ/危機管理",
-    "title": "【要Chromeアクセス】重要度の高い脆弱性情報を更新しました(4/21 3件)",
-    "color": "text-red-500",
-    "url": "http://tokyo.nekonet.co.jp/menu/pageout.aspx?key=Whats&url=https://sites.google.com/nekonet.co.jp/security-portal/要確認脆弱性情報"
   }
 ];
